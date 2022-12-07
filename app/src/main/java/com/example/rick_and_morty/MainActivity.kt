21dad1d43a8319e2097d32259e309e08d5b91f79
Elizaveta <98260898+perepelica99@users.noreply.github.com>
2022-12-07 22:39:30 +0300

    Initial commit

---
+++ app/src/main/java/com/example/rick_and_morty/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.rick_and_morty
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+
+
+        }
+
+
+    }
+}
